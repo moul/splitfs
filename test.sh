72fcf653368f1ca0dfa96d104e109ce27aa44c94
Manfred Touron <github@42.am>
2012-03-21 09:00:41 +0100

    updated test.sh to add more options

--- test.sh
+++ test.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
+BIN="$(pwd)/splitfs.py"
+MOUNTPOINT="$(pwd)/mountpoint/"
+OPTS="target=$(pwd)/target/"
+#EXTRA_ARGS=$EXTRA_ARGS -d
+EXTRA_ARGS="$EXTRA_ARGS -f"
+
+BIN="$(pwd)/cachefs.py"
+OPTS=$OPTS,"cache=$(pwd)/cache/"
+
+
+
 clear
-umount -f $(pwd)/mountpoint
-./splitfs.py ./mountpoint  -o target=target
-mount | grep mountpoint | grep -i fuse
+#kill $(ps auxwww | grep -i split | grep python | awk '{print $2}') 2>/dev/null
+umount -f $MOUNTPOINT
+echo $BIN $MOUNTPOINT -o $OPTS $EXTRA_ARGS
+$BIN $MOUNTPOINT -o $OPTS $EXTRA_ARGS
+sleep .2
+mount | grep $MOUNTPOINT | grep -i fuse
+echo
+ls -l $MOUNTPOINT
+
